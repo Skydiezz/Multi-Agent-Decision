299caf8edf2e6576cca30cb9ca3f5264af88dbf9
Skydiezz <alvimavilaguilherme@gmail.com>
2025-05-12 14:34:13 +0200

    improved non-cooperativeAgent

--- non-cooperativeAgent.py
+++ non-cooperativeAgent.py
@@ -103,7 +103,7 @@ class NonCooperativeAgent(AntStrategy):
         movement_list = self.movements.get(ant_id, [])
         path_found = self.path_found.get(ant_id, False)
         factor = self.factor.get(ant_id, 0)
-        returning = self.returning.get(ant_id, False)
+        
 
         if movement_list:
             last = movement_list[-1]
@@ -133,19 +133,33 @@ class NonCooperativeAgent(AntStrategy):
                 "")
                 self.path_found[ant_id] = True
                 path_found = True
+                self.returning[ant_id] = True
 
 
             if last == "COLONY":
                 self.factor[ant_id] = 0
                 print("Got back to colony")
                 movement_list.pop(-1)
+                self.returning[ant_id] = False
+
+                print("Avant :", movement_list)
+                if self.path_found[ant_id] == True:
+                    for i in range(4):
+                        movement_list.pop(-1)
+
                 if factor < len(movement_list):
-                    movement_list = movement_list[factor:] if factor < -1 else movement_list[:-1]
+                    movement_list = movement_list[factor+1:] if factor < -1 else movement_list[:-1]
                 else:
                     movement_list = []
+
+
                 for i in range(4):
-                    movement_list.pop(-1)
                     movement_list.insert(0, "TURN_LEFT")
+                print("Après :", movement_list)
+
+
+
+                print(movement_list) 
                 weight = len(movement_list)               
                 print(f"GOING BACK TO FOOD, CURRENT PATH WEIGHT : \n" \
                 f"------------------ W => {weight}" \
@@ -154,19 +168,28 @@ class NonCooperativeAgent(AntStrategy):
                 self.path_found[ant_id] = True
                 path_found = True
             
-            print(movement_list)
             print(f"PATH FOUND ? ----> {path_found}")
             self.movements[ant_id] = movement_list
             
         # if a path has been found follow this path
-
+        returning = self.returning.get(ant_id, False)
         actual_factor = self.factor.get(ant_id, 0)
-        if abs(actual_factor) < len(movement_list):
+        if abs(actual_factor) < len(movement_list) and actual_factor >= 0 or abs(actual_factor) <= len(movement_list) and actual_factor < 0:
             move = movement_list[actual_factor]
-        else:
+        elif actual_factor != 0:
             print(">>> PATH END REACHED OR INVALID FACTOR, RESETTING")
             self.path_found[ant_id] = False
             self.factor[ant_id] = 0
+            if actual_factor < 0:
+                for i in range(4):
+                    movement_list.pop(-1)
+            else:
+                for i in range(4):
+                    movement_list.pop(0)
+            self.movements[ant_id] = movement_list
+        else:
+            self.path_found[ant_id] = False
+            self.factor[ant_id] = 0            
 
 
         if self.path_found[ant_id] :
@@ -184,15 +207,15 @@ class NonCooperativeAgent(AntStrategy):
 
             print(f"Following path...   ->> {move}")
             match move:
-                case "MOVE_FORWARD":
+                case "MOVE_FORWARD" | "MOVE FORWARD":
                     return AntAction.MOVE_FORWARD
-                case "TURN_LEFT":
+                case "TURN_LEFT" | "TURN LEFT":
       
                     if returning:
                         return AntAction.TURN_RIGHT
                     else:
                         return AntAction.TURN_LEFT
-                case "TURN_RIGHT":
+                case "TURN_RIGHT" | "TURN RIGHT":
 
                     if returning:
                         return AntAction.TURN_LEFT
@@ -209,7 +232,6 @@ class NonCooperativeAgent(AntStrategy):
                 if terrain == TerrainType.COLONY:
                     if pos[1] > 0:
                           # Colony is ahead in some direction
-
                         movement_list.append("MOVE_FORWARD")
                         self.movements[ant_id] = movement_list
                         return AntAction.MOVE_FORWARD
